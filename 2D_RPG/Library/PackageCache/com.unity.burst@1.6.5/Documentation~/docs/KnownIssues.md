c2d8ba355594e42bde86adb2bf81c7b03afe7e31
junghyonil123 <zhengxuanyi1@gmail.com>
2022-09-21 15:49:40 +0900

    2022_09_23_15:41

--- 2D_RPG/Library/PackageCache/com.unity.burst@1.6.5/Documentation~/docs/KnownIssues.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0d2b39866298258e2730c7cb3e7f06be556c93f3d06983875408fef1260da134
-size 3704
--- 2D_RPG/Library/PackageCache/com.unity.burst@1.6.5/Runtime/DiagnosticId.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:30e68640a5131d9ef7adf59b053b8dadf6a616d2dc3509ea494ec1988f5b5242
-size 9024
